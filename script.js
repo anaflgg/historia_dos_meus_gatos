6e8b6c25a7b278063fe512110c80d76002c63858
anaflgg <anaflaviananias3@gmail.com>
2025-09-28 20:32:22 -0300

    Primeira versão do site dos gatinhos

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const gatoThorImg = document.getElementById('gato-thor');
+    const gatoLunaImg = document.getElementById('gato-luna');
+
+    const wrapperThor = document.getElementById('wrapper-thor');
+    const wrapperLuna = document.getElementById('wrapper-luna');
+    
+    gatoThorImg.addEventListener('click', () => {
+        wrapperThor.classList.add('ativo');
+        wrapperLuna.classList.remove('ativo');
+    });
+
+    gatoLunaImg.addEventListener('click', () => {
+        wrapperLuna.classList.add('ativo');
+        wrapperThor.classList.remove('ativo');
+    });
+
+})
