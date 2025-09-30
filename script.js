5b2d0691b392b731a2c374cdf7fca2a37d0ad8d7
anaflgg <anaflaviananias3@gmail.com>
2025-09-29 23:34:41 -0300

    commit antes do segundo flipcard

--- script.js
+++ script.js
@@ -20,4 +20,16 @@ document.addEventListener('DOMContentLoaded', () => {
         duration: 800,
     });
 
+    const thorCardInner = document.getElementById('thor-card-inner');
+    const btnVirarThor = document.getElementById('btn-virar-thor');
+    const thorCardBack = document.querySelector('#historia-thor .flip-card-back');
+
+    btnVirarThor.addEventListener('click', () => {
+        thorCardInner.classList.toggle('is-flipped');
+    });
+
+    thorCardBack.addEventListener('click', () => {
+        thorCardInner.classList.remove('is-flipped');
+    });
+
 })
